5678583c19133fce02f23bb9e5b97a7e6ee35f76
catharygr <103951447+catharygr@users.noreply.github.com>
2023-10-25 18:36:29 +0100

    Presentando un input con doble click

--- script.js
+++ script.js
@@ -2,6 +2,29 @@ const colorOptions = ['ligth-grey','blue','green','yellow','orange','purple'];
 const colorContainer = document.querySelector('.profile-color-options-container');
 const profileColorOptions = document.querySelector('.profile-color-options');
 const settingBtn = document.querySelector('.setting-btn');
+const profileBioContainer = document.querySelector('.profile-bio-container');
+
+profileBioContainer.addEventListener('dblclick', editBio);
+
+function editBio() {
+  const bio = profileBioContainer.querySelector('p');
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.classList.add('profile-bio-input');
+  input.focus();
+  input.value = bio.textContent;
+  bio.textContent = '';
+
+  profileBioContainer.appendChild(input);
+
+  input.addEventListener('blur', () => {
+    bio.textContent = input.value.trim();
+    profileBioContainer.removeChild(input);
+  })
+}
+
+
+
 
 for (let i = 0; i < colorOptions.length; i++) {
   const colorOption = colorOptions[i];
@@ -11,7 +34,7 @@ for (let i = 0; i < colorOptions.length; i++) {
   li.addEventListener('click', changeColor);
 }
 
-settingBtn.addEventListener("click", toggleColorContainer);
+settingBtn.addEventListener('click', toggleColorContainer);
 
 function toggleColorContainer() {
   profileColorOptions.classList.toggle("show");
@@ -19,8 +42,9 @@ function toggleColorContainer() {
 }
 
 function changeColor(e) {
- console.log(e.target.classList[0]);
  document.body.classList.remove(...colorOptions);
   document.body.classList.add(e.target.classList[0]);
 }
+
+
 
