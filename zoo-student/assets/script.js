039efc9c181e7235ba43594cc643d9fe9bb8c9b4
catharygr <103951447+catharygr@users.noreply.github.com>
2023-10-25 15:58:27 +0100

    Trabajando en js amarrando a la clase de ul para crear los li

--- zoo-student/assets/script.js
+++ zoo-student/assets/script.js
@@ -1,1 +1,8 @@
+const listOfColores = ['ligth-grey','blue','green','yellow','orange','purple'];
 const profileColorOptionsContainer = document.querySelector('.profile-color-options-container');
+
+for (let i = 0; i < listOfColores.length; i++) {
+  const colorOption = listOfColores[i];
+  const li = document.createElement('li');
+  
+}
